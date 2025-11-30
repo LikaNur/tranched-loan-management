ef8a7eb96f48feb71366438865f2a4b53a40e669
Ocean <ocean@192.168.1.111>
2025-11-30 20:34:39 +0000

    chore: setup project structure and types

---
+++ frontend/src/lib/types/loan.ts
@@ -0,0 +1,9 @@
+export interface Loan {
+    id: number;
+    initialDebt: number;
+    paid: number;
+  }
+  
+  export function calculateBalance(loan: Loan): number {
+    return loan.initialDebt - loan.paid;
+  }
